608b8c1ae8e66156e6b78665c223a191e4429d68
Amanda-G <amanda.gunturu@yahoo.com>
2020-07-19 19:26:09 -0400

    api call works.

--- src/app/commonComponents/feature-login-and-registration/login/login-component/login.component.ts
+++ src/app/commonComponents/feature-login-and-registration/login/login-component/login.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {LoginAndRegistrationService} from '../../login-and-registration/login-and-registration.service';
+import {ApiService} from '../../../../dj-store/api-service/api.service';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-login',
@@ -19,8 +21,12 @@ export class LoginComponent implements OnInit {
     ]);
     checkboxFormControl = new FormControl('');
     type: any = 'password';
-    constructor(public loginAndRegistrationService: LoginAndRegistrationService) {
+
+    constructor(public loginAndRegistrationService: LoginAndRegistrationService,
+                public apiService: ApiService,
+                private router: Router) {
     }
+
     ngOnInit(): void {
         this.loginForm = new FormGroup({
             email: this.emailFormControl,
@@ -28,13 +34,17 @@ export class LoginComponent implements OnInit {
             checkbox: this.checkboxFormControl
         });
     }
-    logValues(formGroup: FormGroup): void{
-        console.log(formGroup.controls.email.value);
-        console.log(formGroup.controls.password.value);
+
+    verifyLogin(formGroup: FormGroup): void {
         console.log(formGroup.controls.checkbox.value);
+        this.apiService.getAccount(formGroup.controls.email.value).subscribe((data: any[]) => {
+            if (data[0].password === formGroup.controls.password.value) {
+                this.router.navigate(['home']);
+            }
+        });
     }
 
-    togglePassword(){
+    togglePassword() {
         this.type = this.type === 'password' ? 'text' : 'password';
     }
 }
--- src/app/commonComponents/feature-login-and-registration/registration/registration/registration.component.ts
+++ src/app/commonComponents/feature-login-and-registration/registration/registration/registration.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {LoginAndRegistrationService} from '../../login-and-registration/login-and-registration.service';
+import {ApiService} from '../../../../dj-store/api-service/api.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -21,7 +23,9 @@ export class RegistrationComponent implements OnInit {
     Validators.required
   ]);
   type: any = 'password';
-  constructor(public loginAndRegistrationService: LoginAndRegistrationService) { }
+  constructor(public loginAndRegistrationService: LoginAndRegistrationService,
+              public apiService: ApiService,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.loginForm = new FormGroup( {
@@ -30,10 +34,13 @@ export class RegistrationComponent implements OnInit {
       password: this.passwordFormControl
     });
   }
-  logValues(formGroup: FormGroup): void{
-    console.log(formGroup.controls.name.value);
-    console.log(formGroup.controls.email.value);
-    console.log(formGroup.controls.password.value);
+  createUser(formGroup: FormGroup): void{
+    const user = {name: formGroup.controls.name.value,
+                email: formGroup.controls.email.value,
+                password: formGroup.controls.password.value};
+    console.log(user);
+    this.apiService.createAccount(user).subscribe();
+    this.router.navigate(['home']);
   }
   togglePassword(){
     this.type = this.type === 'password' ? 'text' : 'password';
